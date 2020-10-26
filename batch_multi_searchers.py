ac485586e308025def0495744555f4b4c94f88f0
u43369 <dicksianomelo@gmail.com>
2020-10-26 05:42:15 -0700

    -99 replica 12 again

--- batch_multi_searchers.py
+++ batch_multi_searchers.py
@@ -37,7 +37,7 @@ GAMMA = [0.9997]
 LAMBD = [0.95 ]
 
 ### Dispatch  workers ###
-count = 900
+count = 909
 search_space = itertools.product(AGENTS_PER_NODE,
                     MAX_VS, RADIUS, REWARD_RADIUS, COOLDOWN_TIME, REWARD_FACTORS, COLLISION_VELS, KP, BARRIER, DERIVATIVE_OBS, EVALUATE_BASELINE, NUM_STEP_SAME_INPUT,
                     SCHEDULES, HID_SIZE, NUM_HIDDEN_LAYERS, EXPL_RATE,
