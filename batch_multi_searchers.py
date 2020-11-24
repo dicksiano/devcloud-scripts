5976775928b5cd83f3ca860aede0582325229999
u43369 <dicksianomelo@gmail.com>
2020-11-24 14:40:19 -0800

    benchmarking rand, cp0, cp90, cp-90 for new collision

--- batch_multi_searchers.py
+++ batch_multi_searchers.py
@@ -8,7 +8,7 @@ SEED = [1]
 
 ### AGENT PARAMETERS ###
 MAX_VS = [1.0]
-RADIUS =  [2.0]
+RADIUS =  [0.30]
 REWARD_RADIUS = [2.0]
 COOLDOWN_TIME =  [50]
 REWARD_FACTORS = [1]
@@ -16,7 +16,7 @@ COLLISION_VELS = [2.1]
 KP = [15]
 BARRIER = [ [40, 40, 40] ]
 DERIVATIVE_OBS = [0]
-EVALUATE_BASELINE = [0]
+EVALUATE_BASELINE = [1]
 NUM_STEP_SAME_INPUT = [1]
 
 ### NEURAL NET PARAMETERS ###
@@ -26,7 +26,7 @@ NUM_HIDDEN_LAYERS = [2]
 EXPL_RATE = [-5]
 
 ### PPO PARAMETERS ###
-MAXI_TIMESTEPS = [20000000]
+MAXI_TIMESTEPS = [4000000]
 TIMESTEPS_AB = [512]
 CLIP_PARAM = [ 0.1 ]
 ENT_COEFF = [ 0.01]
@@ -41,13 +41,13 @@ LAMBD = [0.95 ]
 PRIOR = [ 1]
 
 ### ALPHA COLLISION ###
-ALPHA = [86]
+ALPHA = [0, 89, -89]
 
 ### RANDOM ###
 RAND = [0]
 
 ### Dispatch  workers ###
-count = 100100
+count = 2
 search_space = itertools.product(AGENTS_PER_NODE,
                     MAX_VS, RADIUS, REWARD_RADIUS, COOLDOWN_TIME, REWARD_FACTORS, COLLISION_VELS, KP, BARRIER, DERIVATIVE_OBS, EVALUATE_BASELINE, NUM_STEP_SAME_INPUT,
                     SCHEDULES, HID_SIZE, NUM_HIDDEN_LAYERS, EXPL_RATE,
@@ -56,7 +56,7 @@ search_space = itertools.product(AGENTS_PER_NODE,
 
 
 for (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, x, y, z, a1, b1, c1) in search_space:
-    for replica in range(4):
+    for replica in range(1):
         strings = ["qsub -F \"" , 
                     str(count),
                     str(a), 
